817ab8840e92c3fcb45fa2642456a69ae3809cd4
NASUNJIN <sj010618@gmail.com>
2025-01-22 02:25:41 +0900

    feat: 프로필 컴포넌트 생성 (#2)

--- src/components/profile/ProfileImageBox.style.ts
+++ src/components/profile/ProfileImageBox.style.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
-export const Profile = styled.div`
+export const ProfileImageContainer = styled.div<{
+  width: string;
+  height: string;
+}>`
   display: flex;
-  width: 50px;
-  height: 50px;
+  align-items: center;
+  justify-content: center;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
   border-radius: 50%;
-  background-color: black;
+`;
+
+export const ProfileImage = styled.img`
+  border-radius: 50%;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 `;
--- src/components/profile/ProfileImageBox.tsx
+++ src/components/profile/ProfileImageBox.tsx
@@ -1,10 +1,20 @@
-import * as S from './Profile.style';
-import { MdPersonOutline } from 'react-icons/md';
+import * as S from './ProfileImageBox.style';
+import penguin from './penguin.jpeg';
 
-export default function Profile() {
-  return(
-    <S.Profile>
-      <MdPersonOutline />
-    </S.Profile>
+interface ProfileImageBoxProps {
+  // src?: string;
+  // alt?: string;
+  width: string;
+  height: string;
+  className?: string;
+}
+
+const ProfileImageBox: React.FC<ProfileImageBoxProps> = ({ width, height, className }) => {
+  return (
+    <S.ProfileImageContainer className={className} width={width} height={height}>
+      <S.ProfileImage src={penguin} alt="펭귄" />
+    </S.ProfileImageContainer>
   );
 };
+
+export default ProfileImageBox;
